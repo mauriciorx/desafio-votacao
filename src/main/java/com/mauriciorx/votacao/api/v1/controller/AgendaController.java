784bd6f0a5e45867325daf6f998a9115c932fb4d
Mauricio Ribeiro Xavier <mauriciorx.20@gmail.com>
2025-03-27 19:54:48 -0300

    feat: add logs for agenda

--- src/main/java/com/mauriciorx/votacao/api/v1/controller/AgendaController.java
+++ src/main/java/com/mauriciorx/votacao/api/v1/controller/AgendaController.java
@@ -4,10 +4,12 @@ import com.mauriciorx.votacao.api.v1.dto.request.AgendaRequestDTO;
 import com.mauriciorx.votacao.api.v1.service.IAgendaService;
 import com.mauriciorx.votacao.api.v1.swagger.AgendaSwagger;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/agenda")
 @RequiredArgsConstructor
@@ -17,36 +19,48 @@ public class AgendaController implements AgendaSwagger {
 
     @PostMapping("/create")
     public ResponseEntity create(@RequestBody AgendaRequestDTO agendaRequestDTO){
+        log.info("Recebida requisição para criar uma pauta: {}", agendaRequestDTO);
+
         try {
             return ResponseEntity.status(HttpStatus.CREATED).body(agendaService.create(agendaRequestDTO));
         } catch (Exception e) {
+            log.error("Erro ao criar pauta: {}", e.getMessage(), e);
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
     @GetMapping("/{agendaId}")
     public ResponseEntity findById(@PathVariable Long agendaId){
+        log.info("Recebida requisição para buscar pauta ID: {}", agendaId);
+
         try {
             return ResponseEntity.ok().body(agendaService.findById(agendaId));
         } catch (Exception e) {
+            log.error("Erro ao buscar pauta ID: {} - {}", agendaId, e.getMessage(), e);
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
     @GetMapping("/")
     public ResponseEntity findAll(){
+        log.info("Recebida requisição para listar todos as pautas");
+
         try {
             return ResponseEntity.ok().body(agendaService.findAll());
         } catch (Exception e) {
+            log.error("Erro ao listar pautas: {}", e.getMessage(), e);
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
     @GetMapping("/outcome/{agendaId}")
     public ResponseEntity generateVoteOutcome(@PathVariable Long agendaId){
+        log.info("Recebida requisição para gerar resultado dos votos da pauta ID: {}", agendaId);
+
         try {
             return ResponseEntity.ok().body(agendaService.generateVoteOutcome(agendaId));
         } catch (Exception e) {
+            log.error("Erro ao gerar resultado dos votos para a pauta ID: {} : {}", agendaId, e.getMessage(), e);
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
--- src/main/java/com/mauriciorx/votacao/api/v1/service/impl/AgendaService.java
+++ src/main/java/com/mauriciorx/votacao/api/v1/service/impl/AgendaService.java
@@ -4,15 +4,18 @@ import com.mauriciorx.votacao.api.v1.dto.request.AgendaRequestDTO;
 import com.mauriciorx.votacao.api.v1.dto.response.AgendaResponseDTO;
 import com.mauriciorx.votacao.api.v1.dto.response.VoteOutcomeResponseDTO;
 import com.mauriciorx.votacao.api.v1.entity.Agenda;
+import com.mauriciorx.votacao.api.v1.entity.Associate;
 import com.mauriciorx.votacao.api.v1.repository.AgendaRepository;
 import com.mauriciorx.votacao.api.v1.repository.VoteRepository;
 import com.mauriciorx.votacao.api.v1.service.IAgendaService;
 import com.mauriciorx.votacao.exception.AgendaNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AgendaService implements IAgendaService {
@@ -23,34 +26,58 @@ public class AgendaService implements IAgendaService {
 
     @Override
     public AgendaResponseDTO create(AgendaRequestDTO agendaRequestDTO) {
+        log.info("Criando uma nova pauta: {}", agendaRequestDTO.getName());
+
         Agenda agenda = agendaRepository.save(Agenda.builder()
                                                     .name(agendaRequestDTO.getName())
                                                     .description(agendaRequestDTO.getDescription())
                                                     .build());
+
+        log.info("Pauta criada com sucesso: {}", agenda);
+
         return getAgendaResponseDTO( agenda );
     }
 
     @Override
     public AgendaResponseDTO findById(Long id) {
+        log.info("Buscando pauta por ID: {}", id);
+
         return getAgendaResponseDTO(agendaRepository.findById(id)
-                .orElseThrow(() -> new AgendaNotFoundException(id)));
+                .orElseThrow(() -> {
+                    log.warn("Agenda não encontrada para ID: {}", id);
+                    return new AgendaNotFoundException(id);
+                }));
     }
 
     @Override
     public List<AgendaResponseDTO> findAll() {
-        return agendaRepository.findAll().stream().map(agenda -> getAgendaResponseDTO( agenda ) ).toList();
+        log.info("Buscando todas as pautas");
+
+        List<Agenda> agendas = agendaRepository.findAll();
+
+        log.info("{} pautas encontradas", agendas.size());
+
+        return agendas.stream().map(this::getAgendaResponseDTO).toList();
     }
 
     @Override
     public VoteOutcomeResponseDTO generateVoteOutcome(Long agendaId) {
+        log.info("Gerando resultado da votação para a pauta ID: {}", agendaId);
+
         Agenda agenda = agendaRepository.findById(agendaId)
-                            .orElseThrow(() -> new AgendaNotFoundException(agendaId));
+                                        .orElseThrow(() -> {
+                                            log.warn("Agenda não encontrada para ID: {}", agendaId);
+                                            return new AgendaNotFoundException(agendaId);
+                                        });
 
         Long approvedVotes = voteRepository.countApprovedVotes(agenda.getId());
         Long rejectedVotes = voteRepository.countRejectedVotes(agenda.getId());
         Long overall = approvedVotes + rejectedVotes;
 
         String outcome = overall == 0 ? "SEM RESULTADO" : approvedVotes > rejectedVotes ? "APROVADO" : "REJEITADO";
+
+        log.info("Resultado gerado para a pauta '{}': Aprovados: {}, Rejeitados: {}, Total: {}, Resultado: {}",
+                    agenda.getName(), approvedVotes, rejectedVotes, overall, outcome);
 
         return VoteOutcomeResponseDTO.builder()
                 .title(agenda.getName())

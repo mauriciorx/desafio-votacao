b339e5180aaf35340267e2bbbd966d048c131d8d
Mauricio Ribeiro Xavier <mauriciorx.20@gmail.com>
2025-03-28 08:19:38 -0300

    test: set test profile to VotacaoApplicationTests

--- src/test/java/com/mauriciorx/votacao/VotacaoApplicationTests.java
+++ src/test/java/com/mauriciorx/votacao/VotacaoApplicationTests.java
@@ -2,7 +2,9 @@ package com.mauriciorx.votacao;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class VotacaoApplicationTests {
 
